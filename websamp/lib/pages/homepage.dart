9c98be124d5342e34d46f5b15f9d6d0d92b33ff7
kekule15 <onyekachiaugustus111@gmail.com>
2021-01-19 19:11:04 +0100

    nav bar

--- websamp/lib/pages/homepage.dart
+++ websamp/lib/pages/homepage.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'navigation/navbar.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget>[
+          NavigationBar(),
+        ],
+      ),
     );
   }
 }
